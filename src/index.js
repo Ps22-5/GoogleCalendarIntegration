2e88a83915c3b67b2b453b8309dadd49fb644b8b
Ps22-5 <prachisoni.052001@gmail.com>
2024-10-30 12:01:57 +0530

    FirstCommit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createClient } from '@supabase/supabase-js';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "YOUR_SUPABASE_URI",
+  "SUPABASE_API_KEY" 
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
+reportWebVitals();
